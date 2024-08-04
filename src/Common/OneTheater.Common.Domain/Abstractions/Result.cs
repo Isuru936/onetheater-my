67fc95bfc2ad1dfe83a9dbf0e3421ad06f33f6bf
Ashanka <ashankar@triolem.com>
2024-08-04 17:28:00 +0530

    Add validation pipeline behavior

--- src/Common/OneTheater.Common.Domain/Abstractions/Result.cs
+++ src/Common/OneTheater.Common.Domain/Abstractions/Result.cs
@@ -37,5 +37,5 @@ public class Result<TValue> : Result
     public static implicit operator Result<TValue>(TValue? value) =>
         value is not null ? Success(value!) : Failure<TValue>(Error.NullValue);
 
-    public static Result<TValue> ValidateFailure(Error error) => new(default, false, error);
+    public static Result<TValue> ValidationFailure(Error error) => new(default, false, error);
 }
