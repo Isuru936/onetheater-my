50d90270ef9b1c92061714b5d76a0d1a5b9b18f9
Ashanka <ashankar@triolem.com>
2024-08-05 06:35:53 +0530

    Integrate cache

--- src/Modules/Users/OneTheater.Modules.Users.Presentation/UserTypes/GetUserTypes.cs
+++ src/Modules/Users/OneTheater.Modules.Users.Presentation/UserTypes/GetUserTypes.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using OneTheater.Common.Application.Caching;
 using OneTheater.Common.Domain.Abstractions;
 using OneTheater.Common.Presentation.ApiResults;
 using OneTheater.Common.Presentation.Endpoints;
@@ -12,12 +13,25 @@ internal sealed class GetUser : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("usertypes", async (ISender sender) =>
-        {
-            Result<IReadOnlyCollection<UserTypeResponse>> result = await sender.Send(new GetUserTypesQuery());
+        app.MapGet("usertypes", async (ISender sender, ICacheService cacheService) =>
+            {
+                IReadOnlyCollection<UserTypeResponse> userTypeResponses =
+                await cacheService.GetAsync<IReadOnlyCollection<UserTypeResponse>>("userTypes");
 
-            return result.Match(Results.Ok, ApiResults.Problem);
-        })
+                if (userTypeResponses is not null)
+                {
+                    return Results.Ok(userTypeResponses);
+                }
+
+                Result<IReadOnlyCollection<UserTypeResponse>> result = await sender.Send(new GetUserTypesQuery());
+
+                if (result.IsSuccess)
+                {
+                    await cacheService.SetAsync("userTypes", result.Value);
+                }
+
+                return result.Match(Results.Ok, ApiResults.Problem);
+            })
             .WithTags(Tags.UserTypes);
     }
 }
